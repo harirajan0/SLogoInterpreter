c34670ecd19a30cd2743f8c3190bcf970924b54c
Hari <hari.rajan@duke.edu>
2017-05-07 19:49:20 -0700

    img selector for individual turtles for vooga analysis

---
+++ ADDITION.md
@@ -0,0 +1,16 @@
+CompSci 308: SLogo Addition
+===================
+
+*Note: Sorry this wasn't committed prior to writing the code. I accidentally had this in my analysis_hpr4 repository instead. But, I did answer the "Estimation" questions prior to writing the code.*
+
+###Estimation
+
+I think that it shouldn't take me much longer than 30 minutes to complete the frontend addition. I know I will have to edit the CommandPromptView.java class to simply add a button that will open a new window for the user to change the turtle image. Also, I will need to write a new class called TurtleImageChanger.java which opens up a new window for users to view the current images and change the images. I also know that I will need to add a method in SLogoController.java to add a listener to the choice box in TurtleImageChanger.java. Lastly, I will have to add a short method in SLogoData.java to change the image of an individual turtle.
+
+###Review
+
+###Analysis
+
+
+
+
--- src/frontend/TurtleImageChanger.java
+++ src/frontend/TurtleImageChanger.java
@@ -8,21 +8,20 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Observable;
 import java.util.Observer;
-import java.util.TreeMap;
 
 import backend.SLogoData;
-import backend.turtle.Turtle;
 import constants.Constants;
 import javafx.application.Application;
 import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
+import javafx.scene.layout.HBox;
 import javafx.stage.FileChooser;
-import javafx.stage.Stage;
 import javafx.stage.FileChooser.ExtensionFilter;
+import javafx.stage.Stage;
 
 /**
  * @author harirajan
@@ -33,24 +32,16 @@ public class TurtleImageChanger extends Application implements Observer {
 	private Group myRoot;
 	private Scene myScene;
 	private SLogoData mySlogoData;
-	private ChoiceBox<String> myChoiceBox;
-	private TreeMap<String, Turtle> myTurtleMap;
+	private ComboBox<HBox> myComboMax;
 	
-	public TurtleImageChanger(Stage s, SLogoData sd) {
+	public TurtleImageChanger(Stage s, SLogoData sd) throws Exception {
 		myRoot = new Group();
 		myScene = new Scene(myRoot, Constants.TURTLE_IMAGE_SELECTOR_WINDOW_WIDTH, Constants.TURTLE_IMAGE_SELECTOR_WINDOW_HEIGHT, Constants.BACKGROUND_COLOR);
-		try {
-			start(s);
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		myChoiceBox = new ChoiceBox();
-		myTurtleMap = new TreeMap<>();
-		myRoot.getChildren().add(myChoiceBox);
+		start(s);
+		myComboMax = new ComboBox();
 		mySlogoData = sd;
-		steUpTreeMap();
 		setUpChoiceBox();
+		myRoot.getChildren().add(myComboMax);
 	}
 
 	/* (non-Javadoc)
@@ -72,20 +63,18 @@ public class TurtleImageChanger extends Application implements Observer {
 		// TODO Auto-generated method stub
 		SLogoData slogoData = (SLogoData) o;
 		mySlogoData = slogoData;
-		steUpTreeMap();
 		setUpChoiceBox();
 	}
 	
 	private void setUpChoiceBox() {
-		myChoiceBox.setItems(FXCollections.observableList(new ArrayList<>(myTurtleMap.keySet())));
-		myChoiceBox.valueProperty().addListener(e -> changeImage(myChoiceBox.getSelectionModel().getSelectedIndex()));
-	}
-	
-	private void steUpTreeMap() {
-		myTurtleMap.clear();
+		List<HBox> l = new ArrayList<>();
 		for (int i = 0; i < mySlogoData.getTurtles().size(); i++) {
-			myTurtleMap.put("Turtle " + String.valueOf(i), mySlogoData.getTurtles().get(i));
+			HBox hb = new HBox();
+			hb.getChildren().addAll(new Label("Turtle " + String.valueOf(i)), mySlogoData.getTurtles().get(i).getNode());
+			l.add(hb);
 		}
+		myComboMax.setItems(FXCollections.observableList(l));
+		myComboMax.valueProperty().addListener(e -> changeImage(myComboMax.getSelectionModel().getSelectedIndex()));
 	}
 	
 	private void changeImage(int index) {
